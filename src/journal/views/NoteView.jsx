8861938248d43400990d7e097abbe5c07c22e37c
Johann Castillo <jcastvas@emeal.nttdata.com>
2023-05-16 22:36:06 -0500

    Design JournalApp

---
+++ src/journal/views/NoteView.jsx
@@ -0,0 +1,47 @@
+import { SaveOutlined } from "@mui/icons-material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import React from "react";
+import { ImageGallery } from "../components";
+
+export const NoteView = () => {
+  return (
+    <Grid
+      container
+      direction={"row"}
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      sx={{ mb: 1 }}
+    >
+      <Grid item>
+        <Typography fontSize={39} fontWeight={"light"}>
+          28 de agosto, 2023
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Button color="primary" sx={{ p: 2 }}>
+          <SaveOutlined sx={{ fontSize: 20, mr: 1 }} />
+          Guardar
+        </Button>
+      </Grid>
+      <Grid container>
+        <TextField
+          type="text"
+          variant="filled"
+          fullWidth
+          placeholder="Ingrese un título"
+          label="Título"
+          sx={{ border: "none", mb: 1 }}
+        />
+        <TextField
+          type="text"
+          variant="filled"
+          fullWidth
+          multiline
+          placeholder="¿Qupe sucedió en el día de hoy?"
+          minRows={5}
+        />
+      </Grid>
+      <ImageGallery />
+    </Grid>
+  );
+};
